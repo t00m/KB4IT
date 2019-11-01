307fbd310d0d7c777eb334324cfcfc775c733d5b
Tomás Vírseda <tomasvirseda@gmail.com>
2019-11-01 17:10:55 +0100

    Deleted the old structure.

--- install_kb4it.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Install KB4IT locally
-sudo apt install asciidoctor coderay ruby-coderay
-python3 setup.py install --user
-
--- kb4it/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-# Author: Tomás Vírseda <tomasvirseda@gmail.com>
-# License: GPLv3
-# Description: __init__.py
-"""
-
-name = "kb4it"
