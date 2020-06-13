198c6cb69c3e7a01a6eebf61bd538ecff0f33886
Tomás Vírseda <tomasvirseda@gmail.com>
2020-06-13 23:30:57 +0200

    Rename classes for better logging from others programs (like Basico)

--- scripts/install_kb4it.sh
+++ scripts/install_kb4it.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Install KB4IT locally
-sudo apt install asciidoctor coderay ruby-coderay
+sudo apt install asciidoctor coderay ruby-coderay python3-setuptools
 python3 setup.py install --user
