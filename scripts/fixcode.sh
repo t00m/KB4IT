9f9fe886afdd30f1e526d7dca0e5e864034218e3
Tomás Vírseda <tomasvirseda@gmail.com>
2020-07-06 22:38:03 +0200

    Pylinted app.py

--- scripts/fixcode.sh
+++ scripts/fixcode.sh
@@ -2,10 +2,10 @@ echo "Checking coding standard for file: $1"
 echo "================================================================="
 pylint -d C0301 $1
 echo ""
-echo "Checking code style for file: $1"
-echo "================================================================="
-pycodestyle --ignore E501--max-line-length=100 --show-source --statistics $1
-echo ""
-echo "Checking doc style for file: $1"
-echo "================================================================="
-pydocstyle $1
+#~ echo "Checking code style for file: $1"
+#~ echo "================================================================="
+#~ pycodestyle --ignore E501--max-line-length=100 --show-source --statistics $1
+#~ echo ""
+#~ echo "Checking doc style for file: $1"
+#~ echo "================================================================="
+#~ pydocstyle $1
