41d39eb81018731f7a99c9b61feaa8937b4383ed
t00m <tomasvirseda@gmail.com>
2022-03-05 14:43:04 +0100

    Refactor main.py

--- kb4it/main.py
+++ kb4it/main.py
@@ -13,14 +13,17 @@ import os
 import sys
 import json
 import argparse
-import tempfile
-from argparse import Namespace
 from kb4it.core.env import APP, LPATH, GPATH
 from kb4it.core.log import get_logger
 from kb4it.services.backend import Backend
 from kb4it.services.frontend import Frontend
 from kb4it.services.database import Database
 from kb4it.services.builder import Builder
+
+
+def get_version():
+    """Get KB4IT version."""
+    return '%s %s' % (APP['shortname'], APP['version'])
 
 
 class KB4IT:
@@ -35,66 +38,67 @@ class KB4IT:
     tmpdir = None
 
     def __init__(self, params=None):
-        """Initialize KB4IT class."""
+        """Initialize KB4IT class.
+
+        Setup environment.
+        Initialize main log.
+        Register main services.
+        """
         self.params = params
 
         # Initialize log
-        try:
-            self.setup_logging(self.params.LOGLEVEL)
-        except:
-            self.setup_logging(severity='INFO')
-
+        self.__setup_logging(self.params.LOGLEVEL)
         self.log.info("[CONTROLLER] - KB4IT %s started", APP['version'])
         self.log.info("[CONTROLLER] - Log level set to %s", self.params.LOGLEVEL)
 
         # Start up
-        self.setup_environment()
-        self.check_params()
-        self.setup_services()
+        self.__setup_environment()
+        self.__check_params()
+        self.__setup_services()
 
-    def setup_logging(self, severity=None):
+    def __setup_logging(self, severity=None):
         """Set up logging."""
         self.log = get_logger(__class__.__name__, severity.upper())
 
-    def check_params(self):
+    def __check_params(self):
         """Check arguments passed to the application."""
         for key in vars(self.params):
             self.log.debug("[CONTROLLER] - Parameter[%s] Value[%s]", key, vars(self.params)[key])
 
         if not self.params.LIST_THEMES:
             # Get repository configuration path. Mandatory
-            try:
-                repo_path = os.path.abspath(self.params.REPO_PATH)
-                self.log.debug("[CONTROLLER] - Repository configuration path: %s", repo_path)
-                if not os.path.exists(repo_path):
-                    self.log.error("[CONTROLLER] - Repository configuration path doesn't exist.")
-                    self.stop()
-            except Exception as error:
-                self.log.error("[CONTROLLER] - Error: %s", error)
-                self.log.error("[CONTROLLER] - The repository config path is mandatory.")
-                self.log.error("[CONTROLLER] - Error. Repository configuration path is not valid")
+            if self.params.REPO_PATH is None:
+                self.log.error("[CONTROLLER] - Repository config path parameter (-r) is mandatory.")
+                self.stop()
+
+            repo_path = os.path.abspath(self.params.REPO_PATH)
+            self.log.debug("[CONTROLLER] - Repository configuration path: %s", repo_path)
+            if not os.path.exists(repo_path):
+                self.log.error("[CONTROLLER] - Repository configuration path doesn't exist.")
                 self.stop()
 
             with open(repo_path, 'r') as conf:
-                self.repo = json.load(conf)
+                try:
+                    self.repo = json.load(conf)
+                except json.decoder.JSONDecodeError:
+                    self.log.error("[CONTROLLER] - Repository config file couldn't be read")
+                    self.stop()
 
             # Check source and target paths
-            try:
-                title = self.repo['title']
-                source = self.repo['source']
-                target = self.repo['target']
-                theme = self.repo['theme']
-                sort = self.repo['sort']
-            except Exception as error:
-                self.log.error("[CONTROLLER] - Repository configuration is not valid. Check and fix, please:")
-                self.log.error("[CONTROLLER] - It must contain a title, a valid source and target paths, a valid theme name and the sorting property.")
-                self.log.error("[CONTROLLER] - Error: %s", error)
-                self.stop()
+            mandatory = ['title', 'source', 'target', 'theme', 'sort']
+            for key in mandatory:
+                if key in self.repo:
+                    self.log.debug("[CONTROLLER] - Repository Key[%s]: Found!", key)
+                else:
+                    self.log.error("[CONTROLLER] - Repository configuration is not valid. Check and fix, please:")
+                    self.log.error("[CONTROLLER] - It must contain a title, a valid source and target paths, a valid theme name and the sorting property.")
+                    self.log.error("[CONTROLLER] - Key missing: %s", key)
+                    self.stop()
 
-            if source == target:
+            if self.repo['source'] == self.repo['target']:
                 self.log.error("[CONTROLLER] - Error. Source and target paths are the same. That is not possible.")
-                self.log.error("[CONTROLLER] - Source path: %s", source)
-                self.log.error("[CONTROLLER] - Target path: %s", target)
+                self.log.error("[CONTROLLER] - Source path: %s", self.repo['source'])
+                self.log.error("[CONTROLLER] - Target path: %s", self.repo['target'])
                 self.log.error("[CONTROLLER] - Check, please!")
                 self.stop()
 
@@ -111,37 +115,34 @@ class KB4IT:
         """Return repos configuration"""
         return self.repo
 
-    def setup_environment(self):
+    def __setup_environment(self):
         """Set up KB4IT environment."""
         self.log.debug("[CONTROLLER] - Setting up %s environment", APP['shortname'])
         self.log.debug("[CONTROLLER] - Global path[%s]", GPATH['ROOT'])
         self.log.debug("[CONTROLLER] - Local path[%s]", LPATH['ROOT'])
 
         # Create local paths if they do not exist
-        for entry in LPATH:
-            if not os.path.exists(LPATH[entry]):
-                os.makedirs(LPATH[entry])
-                self.log.debug("[CONTROLLER] - Directory[%s] created", LPATH[entry])
+        for key, path in LPATH.items():
+            if not os.path.exists(path):
+                os.makedirs(path)
+                self.log.debug("[CONTROLLER] - LPATH[%s] Dir[%s]: created", key, path)
             else:
-                self.log.debug("[CONTROLLER] - Directory[%s] already exists", LPATH[entry])
+                self.log.debug("[CONTROLLER] - LPATH[%s] Dir[%s]: already exists", key, path)
 
-    def setup_services(self):
+    def __setup_services(self):
         """Declare and register services."""
         self.services = {}
-        try:
-            services = {
-                'DB': Database(),
-                'Backend': Backend(),
-                'Frontend': Frontend(),
-                'Builder': Builder(),
-            }
-            for name in services:
-                self.register_service(name, services[name])
-        except Exception as error:
-            self.log.error("[CONTROLLER] - %s", error)
-            raise
+        services = {
+            'DB': Database(),
+            'Backend': Backend(),
+            'Frontend': Frontend(),
+            'Builder': Builder(),
+        }
+        for name, klass in services.items():
+            self.register_service(name, klass)
 
     def get_services(self):
+        """Get all registered services"""
         return self.services
 
     def get_service(self, name):
@@ -154,7 +155,7 @@ class KB4IT:
             return service
         except KeyError as service:
             self.log.error("[CONTROLLER] - Service %s not registered or not found", service)
-            raise
+            return None
 
     def register_service(self, name, service):
         """Register a new service."""
@@ -193,10 +194,6 @@ class KB4IT:
                 frontend.theme_list()
         self.stop()
 
-    def get_version(self):
-        """Get KB4IT version."""
-        return '%s %s' % (APP['shortname'], APP['version'])
-
     def stop(self):
         """Stop registered services by executing the 'end' method (if any)."""
         try:
@@ -207,6 +204,7 @@ class KB4IT:
             pass
         self.log.debug("[CONTROLLER] - KB4IT %s finished", APP['version'])
         sys.exit()
+
 
 def main():
     """Set up application arguments and execute."""
@@ -221,7 +219,7 @@ def main():
     kb4it_options = parser.add_argument_group('KB4IT Options')
     kb4it_options.add_argument('-r', help='Repository config file', dest='REPO_PATH')
     kb4it_options.add_argument('-l', help='List all installed themes', action='store_true', dest='LIST_THEMES', required=False)
-    kb4it_options.add_argument('-L', help='Control output verbosity. Default to INFO', dest='LOGLEVEL', action='store', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], default='INFO')
+    kb4it_options.add_argument('-L', help='Control output verbosity. Default set to INFO', dest='LOGLEVEL', action='store', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], default='INFO')
     kb4it_options.add_argument('-v', help='Show current version', action='version', version='%s %s' % (APP['shortname'], APP['version']))
 
     params = parser.parse_args()
