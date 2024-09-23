af4d1d26b8218a5a42be6ba57a2f0a5dcf488b29
Tomás Vírseda <tomasvirseda@gmail.com>
2024-09-23 06:45:42 +0200

    chore: install scripts

--- scripts/install_kb4it.sh
+++ scripts/install_kb4it.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Install KB4IT locally
-sudo apt install asciidoctor coderay ruby-coderay python3-setuptools
+#~ sudo apt install asciidoctor coderay ruby-coderay python3-setuptools
 #~ python3 setup.py install --user
+pip uninstall kb4it -y --break-system-packages
+python3 ./scripts/genbuild.py
 pip3 install . --user --break-system-packages
