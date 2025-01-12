6291bb23cb63e6f276aa731333b5ced201c01369
Tomás Vírseda <tomasvirseda@gmail.com>
2025-01-12 23:31:42 +0100

    feat: KB4IT uses pipx for the (un)installation process

--- scripts/install/local/install_kb4it_from_source.sh
+++ scripts/install/local/install_kb4it_from_source.sh
@@ -18,104 +18,31 @@ echo "The latest Python version is: $latest_python"
 
 # Find all installed PIP versions
 latest_pip=$(ls /usr/bin/pip3.* 2>/dev/null | sort -V | tail -n 1)
+echo "PIP: $latest_pip"
 
-# Check if a PIP version was found
-if [[ -z "$latest_pip" ]]; then
-  echo "No PIP 3 versions found."
+# Find all installed PIPX versions
+latest_pipx=$(ls /usr/bin/pipx 2>/dev/null | sort -V | tail -n 1)
+echo "PIPX: $latest_pipx"
+
+# Check if a pipx version was found
+if [[ -z "$latest_pipx" ]]; then
+  echo "pipx not found."
   exit 1
 fi
 
-# Output the latest PIP version
-echo "The latest PIP version is: $latest_pip"
+# Output the latest pipx version
+echo "The latest pipx version is: $latest_pipx"
 
 echo ""
 echo ""
 
 uninstall_kb4it() {
-    if [ -f /etc/os-release ]; then
-        # Read the os-release file
-        . /etc/os-release
-        case "$ID" in
-            debian)
-                echo "Uninstalling KB4IT in Debian"
-                $latest_pip uninstall kb4it -y --break-system-packages
-                ;;
-            ubuntu)
-                echo "Unistalling KB4IT in Ubuntu (based on Debian)"
-                $latest_pip uninstall kb4it -y --break-system-packages
-                ;;
-            rhel | redhat)
-                echo "Uninstalling KB4IT in Red Hat Enterprise Linux"
-                $latest_pip uninstall kb4it -y
-                ;;
-            centos)
-                echo "Uninstalling KB4IT in Fedora"
-                $latest_pip uninstall kb4it -y
-                ;;
-        fedora)
-                echo "Uninstalling KB4IT in CentOS"
-                $latest_pip uninstall kb4it -y
-                ;;
-            arch)
-                echo "Uninstalling KB4IT in Arch Linux"
-                echo "No instructions provided. Open a ticket (and if possible the command):"
-                echo "https://github.com/t00m/KB4IT/issues"
-                ;;
-            *)
-                echo "Unknown Linux distribution: $ID"
-                echo "Open a ticket (and if possible the command):"
-                echo "https://github.com/t00m/KB4IT/issues"
-                ;;
-        esac
-    else
-        echo "/etc/os-release not found. Cannot detect distribution."
-        echo "Open a ticket (and if possible the command):"
-        echo "https://github.com/t00m/KB4IT/issues"
-    fi
+    $latest_pipx uninstall kb4it
 }
 
 
 install_kb4it() {
-    if [ -f /etc/os-release ]; then
-        # Read the os-release file
-        . /etc/os-release
-        case "$ID" in
-            debian)
-                echo "Installing KB4IT in Debian"
-                $latest_pip install . --user --trusted-host files.pythonhosted.org --trusted-host pypi.org --break-system-packages
-                ;;
-            ubuntu)
-                echo "Installing KB4IT in Ubuntu (based on Debian)"
-                $latest_pipinstall . --user --trusted-host files.pythonhosted.org --trusted-host pypi.org --break-system-packages
-                ;;
-            rhel | redhat)
-                echo "Red Hat Enterprise Linux"
-                $latest_pip install . --user --trusted-host files.pythonhosted.org --trusted-host pypi.org
-                ;;
-            centos)
-                echo "CentOS"
-                $latest_pip install . --user --trusted-host files.pythonhosted.org --trusted-host pypi.org
-                ;;
-            fedora)
-                echo "Fedora"
-                $latest_pip install . --user --trusted-host files.pythonhosted.org --trusted-host pypi.org
-                ;;
-            arch)
-                echo "Arch Linux"
-                echo "No instructions provided. Open a ticket (and if possible the command):"
-                echo "https://github.com/t00m/KB4IT/issues"
-                ;;
-            *)
-                echo "Unknown Linux distribution: $ID"
-                echo "Open a ticket (and if possible the command):"
-                echo "https://github.com/t00m/KB4IT/issues"
-                ;;
-        esac
-    else
-        echo "/etc/os-release not found. Cannot detect distribution."
-        echo "Open a ticket (and if possible the command):"
-        echo "https://github.com/t00m/KB4IT/issues"
-    fi
+    $latest_pipx install . --force
 }
 
 echo "KB4IT installer"
