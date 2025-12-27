793b6f309c2ad4c46e044162734f686f178d7a81
Tomás Vírseda <tomasvirseda@gmail.com>
2025-12-27 18:22:38 +0100

    chore: Modify genbuild.py script to update version also in file pyproject.toml

--- scripts/devel/genbuild.py
+++ scripts/devel/genbuild.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import re
 import semver
 
 current = open('kb4it/VERSION', 'r').read()
 print("Current version: %s" % current)
 version = semver.VersionInfo.parse(current).bump_build()
 print("Next build: %s" % version)
+
+# Update main VERSION file
 with open('kb4it/VERSION', 'w') as fv:
     fv.write(str(version))
+print(f"kb4it/VERSION updated to {version}")
 
-print("Update pyproject.toml manually!!")
+# Update pyproject.toml
+pattern = r'^version\s*=\s*"[^"]*"'
+replacement = f'version = "{version}"'
+content = open('pyproject.toml').read()
+new_content, count = re.subn(pattern, replacement, content, flags=re.MULTILINE)
+with open('pyproject.toml', 'w') as fout:
+  fout.write(new_content)
+
+print(f"pyproject.toml updated to {version}")
