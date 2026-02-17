910ac9f207fe82ee6a99c623ef95f6480e00228c
Tomás Vírseda <tomasvirseda@gmail.com>
2026-02-17 22:46:03 +0100

    chore: enable fixcode script

--- scripts/devel/fixcode.sh
+++ scripts/devel/fixcode.sh
@@ -1,11 +1,11 @@
 echo "Checking coding standard for file: $1"
 echo "================================================================="
-pylint -d C0301 $1
+pylint -d "C0301,W0511,C0103,W0201,W1203" $1
 echo ""
-#~ echo "Checking code style for file: $1"
-#~ echo "================================================================="
-#~ pycodestyle --ignore E501--max-line-length=100 --show-source --statistics $1
-#~ echo ""
-#~ echo "Checking doc style for file: $1"
-#~ echo "================================================================="
-#~ pydocstyle $1
+echo "Checking code style for file: $1"
+echo "================================================================="
+pycodestyle --ignore E501 --max-line-length=100 --show-source --statistics $1
+echo ""
+echo "Checking doc style for file: $1"
+echo "================================================================="
+pydocstyle $1
