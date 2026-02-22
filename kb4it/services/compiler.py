60007bdff4863523829ddc77cb7c56ae25f77e8b
Tomás Vírseda <tomasvirseda@gmail.com>
2026-02-22 22:58:40 +0100

    refactor: fixcode.sh for Service Compiler

--- kb4it/services/compiler.py
+++ kb4it/services/compiler.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python
+
 """
+Service Compiler.
+
 # Author: Tomás Vírseda <tomasvirseda@gmail.com>
 # License: GPLv3
 # Description: Cleaner service
 """
 
-import datetime
 import os
+import time
 import random
 import shutil
-import sys
-import time
+import datetime
 import threading
 from concurrent.futures import ThreadPoolExecutor as Executor
-
 
 from kb4it.core.env import ENV
 from kb4it.core.service import Service
 from kb4it.core.util import exec_cmd
 from kb4it.core.util import get_default_workers
-from kb4it.core.util import get_hash_from_file
 from kb4it.core.util import get_source_docs
 
 
 class Compiler(Service):
-    """KB4IT Compiler Service"""
+    """KB4IT Compiler Service."""
 
     def _initialize(self):
-        """Initialize compiler service"""
+        """Initialize compiler service."""
         self.srvbes = self.app.get_service('Backend')
         self.srvthm = self.get_service('Theme')
 
     def execute(self):
         """Compile documents to html with asciidoctor."""
-        self.log.debug(f"[COMPILER] - START")
+        self.log.debug("[COMPILER] - START")
         runtime = self.srvbes.get_dict('runtime')
         dcomps = datetime.datetime.now()
 
         # copy online resources to target path
-        # ~ resources_dir_source = GPATH['THEMES']
         resources_dir_tmp = os.path.join(self.srvbes.get_path('tmp'), 'resources')
-        #if path already exists, remove it before copying with copytree()
+
+        # if path already exists, remove it before copying with copytree()
         if os.path.exists(resources_dir_tmp):
             shutil.rmtree(resources_dir_tmp)
             shutil.copytree(ENV['GPATH']['RESOURCES'], resources_dir_tmp)
@@ -57,9 +57,8 @@ class Compiler(Service):
             else:
                 adocprops += '-a %s ' % prop
         runtime['adocprops'] = adocprops
-        #self.log.debug(f"[COMPILATION] - Parameters passed to Asciidoctor: %s", adocprops)
+        # ~ self.log.debug(f"[COMPILATION] - Parameters passed to Asciidoctor: %s", adocprops)
 
-        # ~ distributed = self.srvthm.get_distributed()
         distributed = self.srvbes.get_value('docs', 'targets')
         max_workers = self.srvbes.get_value('repo', 'workers')
         if max_workers is None:
@@ -70,25 +69,18 @@ class Compiler(Service):
             jobs = []
             jobcount = 0
             num = 1
+
             # ~ self.log.debug(f"Generating jobs")
             for doc in docs:
                 COMPILE = False
                 basename = os.path.basename(doc)
                 if basename in distributed:
                     COMPILE = True
-                    # ~ distributed_file = os.path.join(self.srvbes.get_path('dist'), basename)
-                    # ~ cached_file = os.path.join(self.srvbes.get_path('cache'), basename.replace('.adoc', '.html'))
-                    # ~ if os.path.exists(distributed_file) and os.path.exists(cached_file):
-                    # ~ if os.path.exists(cached_file):
-                        # ~ cached_hash = get_hash_from_file(distributed_file)
-                        # ~ current_hash = get_hash_from_file(doc)
-                        # ~ if cached_hash == current_hash:
-                            # ~ COMPILE = False
 
                 FORCE = self.srvbes.get_value('repo', 'force') or False
                 if COMPILE or FORCE:
                     cmd = "asciidoctor -q -s {} -b html5 -D {} {}".format(adocprops, self.srvbes.get_path('tmp'), doc)
-                    #self.log.debug(f"CMD[%s]", cmd)
+                    # ~ self.log.debug(f"CMD[%s]", cmd)
                     data = (doc, cmd, num)
                     self.log.debug(f"DOC[{basename}] compiles in JOB[{num}]")
                     job = exe.submit(self.compilation_started, data)
@@ -98,7 +90,7 @@ class Compiler(Service):
                 else:
                     self.log.debug(f"DOC[{basename}] cached. Avoid compiling")
 
-            if num-1 > 0:
+            if num - 1 > 0:
                 self.log.debug("[COMPILATION] - START")
                 # ~ self.log.debug(f"[COMPILATION] - %3s%% done", "0")
                 for job in jobs:
@@ -108,7 +100,7 @@ class Compiler(Service):
                     if jobcount % ENV['CONF']['MAX_WORKERS'] == 0:
                         pct = int(jobcount * 100 / len(docs))
                         # ~ self.log.info("[COMPILATION] - %3s%% done", str(pct))
-                        self.log.debug(f"STATS - JOB[{jobid}/{num -1}] Compilation progress: {pct}% done")
+                        self.log.debug(f"STATS - JOB[{jobid}/{num - 1}] Compilation progress: {pct}% done")
 
                 dcompe = datetime.datetime.now()
                 comptime = dcompe - dcomps
@@ -117,7 +109,7 @@ class Compiler(Service):
                     duration = 1
                 avgspeed = int((num - 1) / duration)
                 pct = int(jobcount * 100 / len(docs))
-                self.log.debug(f"STATS - JOB[{jobid}/{num -1}] Compilation progress: {pct}% done")
+                self.log.debug(f"STATS - JOB[{jobid}/{num - 1}] Compilation progress: {pct}% done")
                 self.log.debug(f"STATS - Compilation time: {comptime.seconds} seconds")
                 self.log.debug(f"STATS - Compiled docs: {num - 1}")
                 self.log.debug(f"STATS - Avg. Speed: {avgspeed} docs/sec")
@@ -126,31 +118,29 @@ class Compiler(Service):
             self.log.debug(f"[COMPILATION] - END")
 
     def compilation_started(self, data):
+        """Execute compilation."""
         (doc, cmd, num) = data
         res = exec_cmd(data)
         return res
 
     def compilation_finished(self, future):
+        """Once compiled, build page."""
         time.sleep(random.random())
         cur_thread = threading.current_thread().name
         x = future.result()
         if cur_thread != x:
             path_hdoc, rc, num = x
             basename = os.path.basename(path_hdoc)
-            # ~ self.log.debug(f"[COMPILATION] - Job[%s] for Doc[%s] has RC[%s]", num, basename, rc)
+            self.log.debug(f"[COMPILATION] - Job[%s] for Doc[%s] has RC[%s]", num, basename, rc)
             try:
-                html = self.srvthm.build_page(path_hdoc)
+                self.srvthm.build_page(path_hdoc)
             except MemoryError:
                 self.log.error("Memory exhausted!")
                 self.log.error("Please, consider using less workers or add more memory to your system")
                 self.log.error("The application will exit now...")
                 self.app.stop()
-                sys.exit(errno.ENOMEM)
             except Exception as error:
                 self.log.error(error)
                 self.print_traceback()
                 self.app.stop()
             return x
-
-    def _finalize(self):
-        pass
