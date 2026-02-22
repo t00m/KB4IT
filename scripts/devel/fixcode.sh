3bb564a7f737e1e63a6dab99c2eb1ffdb57ebc60
Tomás Vírseda <tomasvirseda@gmail.com>
2026-02-22 01:06:26 +0100

    chore: update fixcode script

--- scripts/devel/fixcode.sh
+++ scripts/devel/fixcode.sh
@@ -1,6 +1,7 @@
 echo "Checking coding standard for file: $1"
 echo "================================================================="
-pylint -d "C0301,W0511,C0103,W0201,W1203" $1
+#~ pylint -d "C0301,W0511,C0103,W0201,W1203" $1
+pylint -d "W1203" $1
 echo ""
 echo "Checking code style for file: $1"
 echo "================================================================="
